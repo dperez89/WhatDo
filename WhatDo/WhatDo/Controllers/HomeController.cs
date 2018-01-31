342c4906910839e90d7cdeaabc6a3d71e6b3e801
David Perez <d.perez89@aol.com>
2018-01-31 09:29:52 -0600

    adding in logic to the Index action of the Home controller so that the program will return the appropriate view based on the current active user's assigned RoleId

--- WhatDo/WhatDo/Controllers/HomeController.cs
+++ WhatDo/WhatDo/Controllers/HomeController.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhatDo.Models;
 
 namespace WhatDo.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db;
+
+        public HomeController()
+        {
+            db = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
+            var currentUser = db.Users.Find(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            string enjoyerName = "Enjoyer";
+            string adminName = "Admin";                     
+            string enjoyerRoleId = (from role in db.Roles where enjoyerName == role.Name select role.Id).First();
+            string adminId = (from role in db.Roles where adminName == role.Name select role.Id).First();
+            if (currentUser.Roles.First().RoleId == enjoyerRoleId )
+            {
+               return RedirectToAction("Index", "Enjoyer");
+            }
+            if (currentUser.Roles.First().RoleId == adminId)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
             return View();
         }
 
